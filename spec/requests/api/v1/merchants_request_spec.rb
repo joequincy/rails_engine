f5e8085fff8be61522b5965231b7bbaa26b1dca6
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-30 09:00:36 -0600

    Add initial Merchant test

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  it 'sends a list of merchants' do
+    create_list(:merchant, 3)
+    get api_v1_merchants_path
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(items.count).to eq(3)
+  end
+end

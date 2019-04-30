5b8d63e669293cb72d6635540e1568d36d3e1a4a
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-29 20:02:08 -0600

    Add relationship tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'relationships' do
+    it {is_expected.to belong_to :merchant}
+    it {is_expected.to have_many :invoice_items}
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  describe 'relationships' do
+    it {is_expected.to have_many :invoices}
+    it {is_expected.to have_many :items}
+  end
 end

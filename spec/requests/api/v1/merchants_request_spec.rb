d0b6b7dad9fd27dfea87fefa1fd39e51c01b5319
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-04-30 09:31:34 -0600

    Add merchant show

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,10 +5,20 @@ describe 'Merchants API' do
     create_list(:merchant, 3)
     get api_v1_merchants_path
 
-    expect(response).to be_successful
-
     merchants = JSON.parse(response.body)
 
+    expect(response).to be_successful
     expect(merchants.count).to eq(3)
+  end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get api_v1_merchant_path(id)
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(merchant['id']).to eq(id)
   end
 end

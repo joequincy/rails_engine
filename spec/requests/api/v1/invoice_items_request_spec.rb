e6fde3786327274110df27240a2ba6006dbff4bb
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-01 20:56:13 -0600

    Add invoice_item tests

---
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe 'InvoiceItems API Record Endpoints' do
+  it 'sends a list of invoice_items' do
+    create_list(:invoice_item, 3)
+    get '/api/v1/invoice_items'
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice_items['data'].count).to eq(3)
+  end
+
+  it 'can get one invoice_item by its id' do
+    id = create(:invoice_item).id
+    get api_v1_invoice_item_path(id)
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice_item['data']['id'].to_i).to eq(id)
+  end
+
+  it 'can find one invoice_item by its attributes' do
+    invoice_items = create_list(:invoice_item, 3)
+    get api_v1_invoice_items_find_path(id: invoice_items[0].id)
+    invoice_item = JSON.parse(response.body)['data']['attributes']
+
+    expect(response).to be_successful
+    expect(invoice_item['invoice_id']).to eq(invoice_items[0].invoice_id)
+
+    get api_v1_invoice_items_find_path(invoice_id: invoice_items[1].invoice_id)
+    invoice_item = JSON.parse(response.body)['data']['attributes']
+
+    expect(response).to be_successful
+    expect(invoice_item['invoice_id']).to eq(invoice_items[1].invoice_id)
+  end
+
+  it 'can find multiple invoice_items by their attributes' do
+    invoice = create(:invoice)
+    invoice_items = create_list(:invoice_item, 3, invoice: invoice)
+    get api_v1_invoice_items_find_all_path(id: invoice_items[0].id)
+    invoice_item_list = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(invoice_item_list.count).to eq(1)
+    expect(invoice_item_list.first['attributes']['invoice_id']).to eq(invoice_items[0].invoice_id)
+
+    get api_v1_invoice_items_find_all_path(invoice_id: invoice_items[1].invoice_id)
+    invoice_item_list = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+    expect(invoice_item_list.count).to eq(3)
+    expect(invoice_item_list.first['attributes']['invoice_id']).to eq(invoice_items[1].invoice_id)
+  end
+end

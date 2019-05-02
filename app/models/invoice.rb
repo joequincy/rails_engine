173db30bf05160a30829cbc892c002ffb41f5b12
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-01 21:17:15 -0600

    Add missing relationships to models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,5 +4,5 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
-  enum status: [:shipped]
+  enum status: [:pending, :shipped]
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
+  has_one :customer, through: :invoice
   enum result: [:success, :failed]
 end

8f513dfd5be40882db57b3c7d9d618155815e6f4
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-01 22:19:49 -0600

    Add routes for Merchant-related things

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  concern :findable do
+    get 'find_all', to: 'finders#index'
+    get 'find', to: 'finders#show'
+  end
+
   namespace :api do
     namespace :v1 do
+
+      # -------------------- #
+      #      Merchants       #
+      # -------------------- #
       namespace :merchants do
-        get 'find', to: 'finders#show'
+        concerns :findable
+      end
+      resources :merchants, only: [:index, :show] do
+        scope module: :merchants do
+          resources :items, :invoices, :customers_with_pending_invoices, only: [:index]
+        end
+      end
       end
       resources :merchants, only: [:index, :show]
     end

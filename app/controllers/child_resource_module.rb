1114d10f8f0ae184849e30b1e208417a6ec1b055
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-01 21:18:14 -0600

    Add ChildResourceModule with method to get class for current child model

---
+++ app/controllers/child_resource_module.rb
@@ -0,0 +1,12 @@
+module ChildResourceModule
+  private
+
+  def model
+    @model ||= self.class
+                   .name
+                   .demodulize
+                   .sub(/Controller$/,"")
+                   .singularize
+                   .constantize
+  end
+end

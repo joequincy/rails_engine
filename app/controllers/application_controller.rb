a1862179670c51d7f3c44777319d677dae7f1ae5
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-01 21:03:27 -0600

    Add method to get model class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def model
+    @model ||= self.class
+                   .name
+                   .demodulize
+                   .sub(/Controller$/,"")
+                   .singularize
+                   .constantize
+  end
 end

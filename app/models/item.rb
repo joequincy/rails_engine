8edf3ec81213c0265ddef5dd54b0286bb9eeccba
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-05-04 15:38:39 -0700

    Add query for getting an item's best day

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.best_day(id)
+    select("to_char(invoices.created_at, 'YYYY-MM-DD') as best_day")
+    .joins(invoice_items: {invoice: :transactions})
+    .merge(Transaction.successful)
+    .where(items: {id: id})
+    .group('best_day')
+    .order('sum(invoice_items.quantity * invoice_items.unit_price) desc, best_day desc')
+    .take
+  end
 end
